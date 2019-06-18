42ff4bf318482ff0eca91287ba11617c4c6d8226
ritwika314 <mim.ghosh@gmail.com>
2019-06-18 15:19:43 -0500

    testing planned path

--- src/All/basicFollowApp.py
+++ src/All/basicFollowApp.py
@@ -4,7 +4,7 @@ from geometry_msgs.msg import Pose
 
 from agentThread import AgentThread
 from gvh import Gvh
-
+from ..Motion import PlannedPath
 
 class BasicFollowApp(AgentThread):
 
@@ -12,14 +12,20 @@ class BasicFollowApp(AgentThread):
         super(BasicFollowApp, self).__init__(Gvh(pid, num_bots))
         self.start()
 
+
+
     def run(self):
-        while(not self.stopped()):
+        dest1 = Pose()
+        dest1.position.x, dest1.position.y, dest1.position.z = 0., 0., 1.
+        dest2 = Pose()
+        dest2.position.x, dest2.position.y, dest2.position.z = 0., 0., 0.
+        planner = PlannedPath.Planner(self.agent_gvh.moat)
 
-            dest1 = Pose()
-            dest1.position.x, dest1.position.y, dest1.position.z = 0., 0., 1.
-            land = Pose()
-            land.position.x, land.position.y, land.position.z = 0., 0., 0.
 
-            self.agent_gvh.moat.goTo(dest1)
+        while not self.stopped():
 
-            self.agent_gvh.moat.goTo(land)
+
+            pp = planner.planned_path((self.agent_gvh.moat.position.x,self.agent_gvh.moat.position.y),(dest1.position.x, dest1.position.y))
+            planner.follow_path(pp)
+
+
